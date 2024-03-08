b715f32e7a06ec04a3ef9894de0fb6b9273728c6
Sahil Hussain <mdsahil37621@gmail.com>
2024-03-08 20:01:26 +0530

    first commit

---
+++ mole.js
@@ -0,0 +1,72 @@
+let currentMoleTile;
+let currentPlantTile;
+let score = 0;
+let gameOver = false;
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    for(let i=0; i<9; i++) {
+        let tile = document.createElement("div");
+        tile.id = i.toString();
+        tile.addEventListener("click", selectTile);
+        document.getElementById("board").appendChild(tile);
+    }
+    setInterval(setMole, 1000);
+    setInterval(setPlant, 2000);
+}
+function getRandomTile(){
+    let num = Math.floor(Math.random() * 9);
+    return num.toString();
+}
+function setMole(){
+    if(gameOver) {
+        return;
+    }
+    if (currentMoleTile) {
+        currentMoleTile.innerHTML = "";
+    }
+    let mole = document.createElement("img");
+    mole.src = "./img/monty-mole.png";
+
+    let num = getRandomTile();
+    if(currentPlantTile && currentPlantTile.id == num) {
+        return;
+    }
+    
+    currentMoleTile = document.getElementById(num);
+    currentMoleTile.appendChild(mole);
+}
+function setPlant(){
+    if(gameOver) {
+        return;
+    }
+    if (currentPlantTile) {
+        currentPlantTile.innerHTML = "";
+    }
+    let plant = document.createElement("img");
+    plant.src = "/img/piranha-plant.png";
+
+    let num = getRandomTile();
+    if(currentMoleTile && currentMoleTile.id == num) {
+        return;
+    }
+
+    currentPlantTile = document.getElementById(num);
+    currentPlantTile.appendChild(plant);
+}
+function selectTile(){
+    if(gameOver){
+        return;
+    }
+    if(this == currentMoleTile){
+        score += 10;
+        document.getElementById("score").innerText = score.toString();
+    }
+    else if(this == currentPlantTile){
+        document.getElementById("score").innerText = "GAME OVER: " + score.toString();
+        gameOver = true;
+    }
+}
