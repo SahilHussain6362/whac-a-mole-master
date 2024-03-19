640a020fecf24ce454ef691dd7e12542d26c033b
Sahil Hussain <mdsahil37621@gmail.com>
2024-03-19 09:07:46 +0530

    add start and pause button

--- mole.js
+++ mole.js
@@ -3,7 +3,8 @@ let currentPlantTile;
 let score = 0;
 let gameOver = false;
 let clicked = false;
-
+let flag = false;
+let mole, plant;
 window.onload = function() {
     setGame();
 }
@@ -15,8 +16,32 @@ function setGame() {
         tile.addEventListener("click", selectTile);
         document.getElementById("board").appendChild(tile);
     }
-    setInterval(setMole, 1000);
-    setInterval(setPlant, 2000);
+}
+function startGame(){
+    console.log("Game starting...");
+    let status = document.getElementById("play").innerText;
+    console.log(status);
+    if(status === "Play" || status === "Resume"){
+        document.getElementById("play").innerHTML = "<h2>Pause</h2>";
+        flag = true;
+    }
+    else if (status === "Pause"){
+        clearInterval(mole);
+        clearInterval(plant);
+        document.getElementById("play").innerHTML = "<h2>Resume</h2>";
+        flag = false;
+    }
+    else if (status === "Play Again"){
+        document.getElementById("play").textContent = "<h2>Pause</h2>";
+        score = 0;
+        document.getElementById("score").innerText = score.toString();
+        gameOver = false;
+        flag = true;
+    }
+    if(flag){
+        mole = setInterval(setMole, 1000);
+        plant = setInterval(setPlant, 2000);
+    }
 }
 function getRandomTile(){
     let num = Math.floor(Math.random() * 9);
@@ -59,7 +84,7 @@ function setPlant(){
     currentPlantTile.appendChild(plant);
 }
 function selectTile(){
-    if(gameOver){
+    if(gameOver || flag === false){
         return;
     }
     if(this == currentMoleTile && clicked == false){
@@ -69,7 +94,10 @@ function selectTile(){
     }
     else if(this == currentPlantTile){
         document.getElementById("score").innerText = "GAME OVER: " + score.toString();
+        document.getElementById("play").innerText = "Play Again";
         gameOver = true;
+        clearInterval(mole);
+        clearInterval(plant);
     }
 }
 function getDiv(pressed){
@@ -89,6 +117,7 @@ addEventListener("keypress", function(event){
     }
     else if (pressed == currentPlantTile.id){
         document.getElementById("score").innerText = "GAME OVER: " + score.toString();
+        document.getElementById("play").innerText = "Play Again";
         gameOver = true;
     }
 })
